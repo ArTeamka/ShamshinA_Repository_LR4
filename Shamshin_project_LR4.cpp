e633c93b2365eabf137b4e9c24ca0b9031b51824
ArTeamka <ar_team@bk.ru>
2025-04-09 13:50:10 +0300

    Initial commit: skeleton for variant 21

---
+++ Shamshin_project_LR4.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <functional>
+using namespace std;
+
+// Глобальные переменные для хранения введенных данных
+char ch1, ch2;
+
+// Проверка корректности ввода
+bool isValidCharInput(const string& input) {
+    return input.length() == 1;
+}
+
+// Функция ввода первого символа
+void enterChar1() {
+    // Разрабатывается Developer1 - ветка branch_fun_1
+}
+
+// Функция ввода второго символа
+void enterChar2() {
+    // Разрабатывается Developer2 - ветка branch_fun_2
+}
+
+// Функция вывода символов и их кодов ASCII
+void printCharsAndCodes() {
+    // Разрабатывается Developer3 - ветка branch_fun_3
+}
+
+// Функция вывода суммы кодов ASCII
+void printSumOfCodes() {
+    // Разрабатывается Developer4 - ветка branch_fun_4
+}
+
+// Структура пункта меню
+struct MenuItem {
+    string title;
+    function<void()> action;
+};
+
+int main() {
+    map<int, MenuItem> menu = {
+        {1, {"Ввод первого символа", enterChar1}},
+        {2, {"Ввод второго символа", enterChar2}},
+        {3, {"Вывести символы и их коды ASCII", printCharsAndCodes}},
+        {4, {"Вывести сумму кодов ASCII", printSumOfCodes}}
+    };
+
+    int choice = 0;
+    while (true) {
+        cout << "Меню:" << endl;
+        for (const auto& item : menu) {
+            cout << item.first << ". " << item.second.title << endl;
+        }
+        cout << "0. Выход" << endl;
+        cout << "Выберите пункт меню: ";
+        cin >> choice;
+        cin.ignore(); // Очистка буфера ввода
+        
+        if (choice == 0) {
+            cout << "Работа программы завершена." << endl;
+            break;
+        }
+        
+        if (menu.find(choice) != menu.end()) {
+            menu[choice].action();
+        } else {
+            cout << "Неверный пункт меню!" << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
